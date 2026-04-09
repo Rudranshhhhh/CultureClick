8929de44acf45af75f93db0065dae261190d82fb
iambot836 <kishanrajblr836@gmail.com>
2026-04-09 07:17:00 +0530

    adding board page

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 import traceback
+import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
@@ -46,4 +47,6 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
